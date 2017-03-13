3acf0c1fad57ed6668548f02980f6464e66418fd
Mattioo94 <mattioo@windowslive.com>
2017-03-14 00:16:50 +0100

    Dodanie strony startowej. Należy ustawić w XAML poprawną ścieżkę do pliku z logo o nazwie "guziec.png" znajdującego się w podfolderze projektu o nazwie "images"

--- GuziecSIM/GuziecSIM/MainWindow.xaml.cs
+++ GuziecSIM/GuziecSIM/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using klasa_zabezpieczen;
+using Microsoft.Win32;
+using System.IO;
+
 namespace GuziecSIM
 {
     /// <summary>
@@ -20,9 +23,59 @@ namespace GuziecSIM
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _login;
+        private string _klucz;     
+
         public MainWindow()
         {
             InitializeComponent();
+
+            label.ToolTip = "np. Guziec94";
+            label1.ToolTip = "Klucz prywatny przypisany do konta";
+        }
+
+        /* [WCZYTYWANIE PLIKU Z KLUCZEM PRYWATNYM DO ZALOGOWANIA] */
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                label2.Content = openFileDialog.SafeFileName;
+                button.Content = "Zmień";
+
+                _klucz = File.ReadAllText(openFileDialog.FileName);
+                button.BorderBrush = new SolidColorBrush(Color.FromArgb(255, (byte)230, (byte)230, (byte)230));
+            }
+        }
+
+        /* [WYKRYTO WPROWADZANIE ZMIAN W POLU LOGINU] */
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _login = textBox.Text;
+            textBox.BorderBrush = new SolidColorBrush(Color.FromArgb(255, (byte)230, (byte)230, (byte)230));
+        }
+
+        /* [ZAPOCZĄTKOWANIE PRÓBY LOGOWANIA] */
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_login))
+            {
+                if (!string.IsNullOrEmpty(_klucz))
+                {
+                    MessageBox.Show("... Rozpoczynamy proces logowania");
+                }
+                else button.BorderBrush = new SolidColorBrush(Color.FromArgb(255, (byte)242, (byte)202, (byte)202));
+            }
+            else textBox.BorderBrush = new SolidColorBrush(Color.FromArgb(255, (byte)242, (byte)202, (byte)202));
+        }
+
+        private void label3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("... Proces tworzenia konta");
         }
     }
 }
